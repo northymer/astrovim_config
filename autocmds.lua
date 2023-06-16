361591d76fa5ea0733d57e8c10a2be1615952447
northymer <cons.kirilin@gmail.com>
2023-06-16 18:03:42 +0400

    added autosave on focusLost, WinLeave

--- autocmds.lua
+++ autocmds.lua
@@ -1,4 +1,9 @@
 vim.api.nvim_create_autocmd(
   { "FocusGained", "BufEnter" },
-  { pattern = {'*'}, command = ":silent! !" }
+  { pattern = { '*' }, command = ":silent! !" }
+)
+
+vim.api.nvim_create_autocmd(
+  { "FocusLost", "WinLeave" },
+  { pattern = { '*' }, command = ":silent! w" }
 )
