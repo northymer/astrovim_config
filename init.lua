e66143df5cb9deaefac22e10385b7c27eff1f989
Konstantin Kirilin <konstantin.kirilin@idt.net>
2023-06-16 17:34:09 +0400

    added buffer reload

--- init.lua
+++ init.lua
@@ -5,6 +5,19 @@ return {
         "tsserver",
       },
     },
+    setup_handlers = {
+      -- add custom handler
+      tsserver = function(_, opts) require("typescript").setup { server = opts } end
+    }
+  },
+  plugins = {
+    "jose-elias-alvarez/typescript.nvim", -- add lsp plugin
+    {
+      "williamboman/mason-lspconfig.nvim",
+      opts = {
+        ensure_installed = { "tsserver" }, -- automatically install lsp
+      },
+    },
   },
 
   options = {
