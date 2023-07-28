46e1dd5dfbcc9da3459408c608a1f36b9fbbaac4
Konstantin Kirilin <konstantin.kirilin@idt.net>
2023-07-28 15:48:57 +0400

    added trouble (will set up later)

---
+++ plugins/init.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+  {
+    "jose-elias-alvarez/typescript.nvim",
+    disable_commands = false, -- prevent the plugin from creating Vim commands
+    debug = false, -- enable debug logging for commands
+    go_to_source_definition = {
+      fallback = true, -- fall back to standard LSP definition on failure
+    },
+  }
+}
